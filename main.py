5786ac8ffcd60d2617a70a5e31f1e68e60cb1d04
sealoong <48373656+sealoongleft@users.noreply.github.com>
2022-07-08 21:32:33 +0800

    Add files via upload
    Added a new pattern style (a rectangle, and a progress bar)

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
-'''
-实验名称：OLED显示屏（I2C总线）
-版本：v1.0
-日期：2021.1
-作者：01Studio
-社区：www.01studio.org
-'''
 
 
-from machine import SoftI2C,Pin         #从machine模块导入I2C、Pin子模块
-from ssd1306 import SSD1306_I2C     #从ssd1306模块中导入SSD1306_I2C子模块
 
-i2c = SoftI2C(scl=Pin(10), sda=Pin(11))   #SoftI2C初始化：scl--> 10, sda --> 11
-oled = SSD1306_I2C(128, 64, i2c, addr=0x3c) #OLED显示屏初始化：128*64分辨率,OLED的I2C地址是0x3c
+from machine import SPI,Pin         #从machine模块导入I2C、Pin子模块
+from ssd1306 import SSD1306_SPI
+from time    import sleep
+import framebuf
 
-oled.text("Hello World!", 0,  0)      #写入第1行内容
-oled.text("MicroPython",  0, 20)      #写入第2行内容
-oled.text("By 01Studio",  0, 50)      #写入第3行内容
+spi = SPI(0, sck=Pin(2), mosi=Pin(3)) 
+oled = SSD1306_SPI(128, 32, spi, res=Pin(0), dc=Pin(4), cs=Pin(1))
+second=0
+minute=22
+hour  =11
 
-oled.show()   #OLED执行显示
+while True:
+    for i in range(1,5):
+        a=i*32
+        oled.text("stM32L031K6",  10, 0)
+        print(a)
+        oled.text(f"time {hour}:{minute} {second}",  0, 8)
+        oled.text("temp:26.5'C",  0, 17)
+        oled.rect(118,0,10,10,second%3)
+        oled.fill_rect(0,28,a,5,1)
+        oled.scroll(0,0)
+        oled.show()
+        sleep(1)
+        oled.fill(0)
+        oled.show()
+        second+=1
+        if   second==60:
+             second=0
+             minute+=1
+        elif minute==60:
+             minute=0
+             hour+=1
+        elif hour==24:
+             hour=0
+    
+       
+    
+    
+    
+   
+ 
--- ssd1306.py
+++ ssd1306.py
@@ -114,7 +114,7 @@ class SSD1306_I2C(SSD1306):
 
 class SSD1306_SPI(SSD1306):
     def __init__(self, width, height, spi, dc, res, cs, external_vcc=False):
-        self.rate = 10 * 1024 * 1024
+        self.rate =  10*1024 * 1024
         dc.init(dc.OUT, value=0)
         res.init(res.OUT, value=0)
         cs.init(cs.OUT, value=1)
